2ff9191f8f1939ae209fab99962157a2a9e87450
Anders Lundström <anders@urkraft.se>
2016-06-29 17:24:32 +0200

    some router example code, but switch to webpack anyway

--- static/src/Main.purs
+++ static/src/Main.purs
@@ -1,16 +1,28 @@
 module Main where
 
-import AjaxExample.Todos (init, update, view)
-import Prelude (bind)
+import Prelude ((<<<), bind)
 import Pux (start, renderToDOM)
 
+import Pux (start, fromSimple, renderToDOM)
+import Pux.Router (sampleUrl)
+import Signal ((~>))
+
+import RoutingExample.Routes (match)
+import RoutingExample.App (Action(PageView), init, update, view)
+-- import AjaxExample.Todos (init, update, view)
 
 main = do
+  urlSignal <- sampleUrl
+  -- app <- start
+  let routeSignal = urlSignal ~> (PageView <<< match)
+
   app <- start
     { initialState: init
-    , update:       update
-    , view:         view
-    , inputs:       []
+    , update: fromSimple update
+    -- , update:       update
+    , view: view
+    , inputs: [routeSignal]
+    -- , inputs:       []
     }
 
   renderToDOM "#app" app.html
