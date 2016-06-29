014c21014c9718779605ab1097c675920510be25
Anders Lundström <anders@urkraft.se>
2016-06-29 09:42:11 +0200

    renamed database to biblio again and added the user_id to books

--- mysql.sql
+++ mysql.sql
@@ -1,15 +1,21 @@
-create database Books;
-grant all on Books.* on Books@localhost identified by 'Books';
+#drop database biblio;
+create database biblio;
+grant all on biblio.* to `biblio`@`localhost` identified by 'biblio';
 
-flush privileges;
+Flush privileges;
 
-use Books;
+use biblio;
 create table `user` (id int auto_increment primary key, login varchar(128) unique not null, password varchar(128) not null);
 
--- The `user` table contains one user with login 'user' and password 'password'
+
 insert into `user`(`login`, `password`) values('user', md5('my-salt-123password'));
 
 
-create table `book` (id int auto_increment primary key, title varchar(128) unique not null, author varchar(128) not null, content varchar(128) not null);
+create table book(id int auto_increment primary key,
+                  title varchar(128) unique not null,
+                  author varchar(128) not null,
+                  content varchar(128) not null,
+                  year int,
+                  user_id int);
 
-insert into `book`(`title`, `author`, `content`) values('Min Bok', 'Jag', 'Bokens innehåll.');
+insert into book(`title`, `author`, `content`, `year`, `user_id`) values('Min Bok', 'Jag', 'Bokens innehåll.', 2016, 1);
