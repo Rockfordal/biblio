d7c417f6d855a2f9a83b2b6923f80fe9757dc955
Anders Lundström <anders@urkraft.se>
2016-07-23 15:30:43 +0200

    mysql uppdaterad

--- mysql.sql
+++ mysql.sql
@@ -1,52 +1,49 @@
-#drop database biblio;
-create database biblio;
+-- drop database biblio;
+CREATE database biblio;
 grant all on biblio.* to `biblio`@`localhost` identified by 'biblio';
 
 Flush privileges;
 use biblio;
 
+-- SERIAL is an alias for BIGINT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE.
 
--- Användare
-create table `user` (id int auto_increment primary key,
-                     login varchar(128) unique not null,
-                     password varchar(128) not null,
-                     role char(15));
-
-insert into `user`(`login`, `password`) values('user', md5('my-salt-123password'));
-
-
--- Hyllor
-create table shelf(id int auto_increment primary key,
-                  name varchar(128) unique not null,
-                  size int);
-                  -- room_id int
-
-insert into shelf(`name`, `size`) values('A01', 10);
+CREATE TABLE user (
+       id INT UNSIGNED auto_increment PRIMARY KEY,
+       login VARCHAR(128) UNIQUE NOT NULL,
+       password VARCHAR(128) NOT NULL,
+       role CHAR(15)
+       );
 
 
--- Föremål
-create table item(id int auto_increment primary key,
-                name varchar(128) unique not null,
-                info varchar(128) not null);
+CREATE TABLE shelf (
+       id INT UNSIGNED auto_increment PRIMARY KEY,
+       name VARCHAR(128) UNIQUE NOT NULL,
+       size INT
+       -- room_id INT
+       );
 
-insert into item(`name`, `info`) values('Träskruv', 'Plattskalle');
+CREATE TABLE item (
+       id INT UNSIGNED auto_increment PRIMARY KEY,
+       name VARCHAR(128) UNIQUE NOT NULL,
+       info VARCHAR(128) NOT NULL
+       );
 
+CREATE TABLE shelfitem (
+       id INT UNSIGNED auto_increment PRIMARY KEY,
+       quantity INT,
+       shelf_id INT UNSIGNED REFERENCES shelf(id),
+       item_id  INT UNSIGNED REFERENCES item(id),
+       );
 
--- Inventarie Hyllor<->Items
-create table shelfitem(id int auto_increment primary key,
-                quantity int,
-                shelf_id int,
-                item_id  int);
+-- CREATE TABLE book(id INT auto_increment PRIMARY KEY,
+--                   title VARCHAR(128) UNIQUE NOT NULL,
+--                   author VARCHAR(128) NOT NULL,
+--                   content VARCHAR(128) NOT NULL,
+--                   year INT,
+--                   user_id INT);
 
-insert into shelfitem(`shelf_id`, `item_id`, `quantity`) values(1, 1, 20);
-
-
--- Böcker
--- create table book(id int auto_increment primary key,
---                   title varchar(128) unique not null,
---                   author varchar(128) not null,
---                   content varchar(128) not null,
---                   year int,
---                   user_id int);
-
--- insert into book(`title`, `author`, `content`, `year`, `user_id`) values('Min Bok', 'Jag', 'Bokens innehåll.', 2016, 1);
+INSERT INTo user(login, password) values('user', md5('my-salt-123password'));
+INSERT INTo shelf(name, size) values('A01', 10);
+INSERT INTo item(name, info) values('Träskruv', 'Plattskalle');
+INSERT INTo shelfitem(shelf_id, item_id, quantity) values(1, 1, 20);
+-- INSERT INTo book(`title`, `author`, `content`, `year`, `user_id`) values('Min Bok', 'Jag', 'Bokens innehåll.', 2016, 1);
